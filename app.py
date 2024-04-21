deef36a21081f45c79f7f539d61c7675d2492043
chelsm <chesleymillo@gmail.com>
2024-04-21 20:55:00 +0200

    👌 IMPROVE: add exist condition

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+has_data_clean = False
+
 def set_page_config():
     st.set_page_config(
         page_title="Streamlit Airbnb App",
@@ -49,32 +51,38 @@ def main():
     data_csv = load_data("data.csv")
     display_data(data_csv, "Airbnb Data")
 
-    data_clean_csv = load_data("data_clean.csv")
-    display_data(data_clean_csv, "Clean Airbnb Data")
+    try:
+        data_clean_csv = load_data("data_clean.csv")
 
-    st.write('------------------------------------------------------------------------------------------------------------------')
+        if data_clean_csv is not None and not data_clean_csv.empty:
+            display_data(data_clean_csv, "Clean Airbnb Data")
+            
+            st.write('------------------------------------------------------------------------------------------------------------------')
 
-    st.subheader("Get Airbnb Listings from API")
+            st.subheader("Get Airbnb Listings from API")
 
-    data_clean_no_categorised_csv = inverse_categorical(data_csv, 'property_type')
+            city = st.text_input("Enter a city name:", "NYC")
+            submit = st.button("Search")
 
-    city = st.text_input("Enter a city name:", "NYC")
-    submit = st.button("Search")
-
-    if submit:
-        filtered_data = data_clean_csv[data_clean_csv['city'] == city]
-        if not filtered_data.empty:
-            st.write("**Here are the top 10 listings:**")
-            for index, row in filtered_data.head(10).iterrows():
-                property_type =  inverse_categorical(data_csv, 'property_type')[str(row['property_type'])]
-                name = row['name']
-                price = row['log_price']
-                currency = '$'
-                st.write(f"**Name:** {name}")
-                st.write(f"- Type: {property_type}")
-                st.write(f"- Price: {price} {currency}")
+            if submit:
+                filtered_data = data_clean_csv[data_clean_csv['city'] == city]
+                if not filtered_data.empty:
+                    st.write("**Here are the top 10 listings:**")
+                    for index, row in filtered_data.head(10).iterrows():
+                        property_type = inverse_categorical(data_csv, 'property_type').get(str(row['property_type']), 'Unknown')
+                        name = row['name']
+                        price = row['log_price']
+                        currency = '$'
+                        st.write(f"**Name:** {name}")
+                        st.write(f"- Type: {property_type}")
+                        st.write(f"- Price: {price} {currency}")
+                else:
+                    st.write("No listings found for the specified city.")
         else:
-            st.write("No listings found for the specified city.")
+            st.write("No data available.")
+    except FileNotFoundError:
+        st.write("The file 'data_clean.csv' was not found. Please execute the data cleaning script first.")
+
 
 if __name__ == "__main__":
     main()
